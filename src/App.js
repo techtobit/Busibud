ef7b28945505b56f489c81b7d221d3aafbe2436c
MD ASHRAF UDDIN <43129513+techtobit@users.noreply.github.com>
2022-10-28 22:39:45 +0600

    nav+herobanner-basic_setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HeroSection from './Components/Home/HeroSection/HeroSection';
 import NavBar from './Components/Shared/Nav/NavBar';
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <NavBar />
+      <HeroSection />
     </div>
   );
 }
--- src/Components/Shared/Nav/NavBar.js
+++ src/Components/Shared/Nav/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Nav, NavDropdown } from 'react-bootstrap';
 
 const NavBar = () => {
  return (
@@ -17,11 +17,17 @@ const NavBar = () => {
     <Nav.Item>
      <Nav.Link eventKey="link-2">Link</Nav.Link>
     </Nav.Item>
-    <Nav.Item>
-     <Nav.Link eventKey="disabled" disabled>
-      Disabled
-     </Nav.Link>
-    </Nav.Item>
+    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+     <NavDropdown.Item href="#action/3.2">
+      Another action
+     </NavDropdown.Item>
+     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+     <NavDropdown.Divider />
+     <NavDropdown.Item href="#action/3.4">
+      Separated link
+     </NavDropdown.Item>
+    </NavDropdown>
    </Nav>
   </div>
  );
