3f1d440e50b3cc48c57182a52ad5a73246fe6454
MD ASHRAF UDDIN <43129513+techtobit@users.noreply.github.com>
2022-10-28 19:01:45 +0600

    app setup

---
+++ src/Components/Shared/Nav/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+
+const NavBar = () => {
+ return (
+  <div className='d-flex align-items-center justify-content-center'>
+   <Nav
+    activeKey="/home"
+    onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+   >
+    <Nav.Item>
+     <Nav.Link href="/home">Active</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+     <Nav.Link eventKey="link-1">Link</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+     <Nav.Link eventKey="link-2">Link</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+     <Nav.Link eventKey="disabled" disabled>
+      Disabled
+     </Nav.Link>
+    </Nav.Item>
+   </Nav>
+  </div>
+ );
+};
+
+export default NavBar;
