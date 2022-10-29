430a96d618a9d921a31000b7ff548c614c38b46c
MD ASHRAF UDDIN <43129513+techtobit@users.noreply.github.com>
2022-10-29 18:16:32 +0600

    sectionTowDone+responsive

--- src/Components/Home/SectionTwo.js
+++ src/Components/Home/SectionTwo.js
@@ -1,58 +1,52 @@
 import React from 'react';
-import { Card, Row } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import '../../Style/Main.scss';
 
 const SectionTwo = () => {
  return (
   <div className="sectionTwo">
-   <div className="section-title text-center">
-    <h2>Busidud Upcoming Services</h2>
-   </div>
+   {/* <div className="section-title text-center">
+    <h2>Lorem ipsum dolor sit amet,consectetur adipiscing elit.</h2>
+   </div> */}
 
-   <div className=' container-full d-grid row justify-between justify-content-center'>
-
-    <Card className='sectionTow-cards'>
+   <div className='section-cards p-0 m-0 container-full row justify-center justify-content-center'>
+    {/* card one */}
+    <Card className='sectionTow-card shadow rounded-3 border-0 hover-border text-center' >
      <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-      <Card.Text>
-       Some quick example text to build on the card title and make up the
-       bulk of the card's content.
+      <Card.Subtitle className='card-subtitle fw-bold py-3'>Lorem ipsum dolor sit .</Card.Subtitle>
+      <Card.Text className='card-text'>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed commodo
+       est.Pellentesque eu placerat mauris. Etiam ut maximus ante. Suspendisse
+       varius dolor nec bibendum dignissim. Proin in diam a justo sollicitudin
+       mattis eu sagittisnunc. In hac habitasse.
       </Card.Text>
-      <Card.Link href="#">Card Link</Card.Link>
-      <Card.Link href="#">Another Link</Card.Link>
      </Card.Body>
     </Card>
-
     {/* card two */}
-    <Card className='sectionTow-cards'>
+    <Card className='sectionTow-card shadow rounded-3 border-0 text-center'>
      <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-      <Card.Text>
-       Some quick example text to build on the card title and make up the
-       bulk of the card's content.
+      <Card.Subtitle className='card-subtitle fw-bold  py-3'>Lorem ipsum dolor sit .</Card.Subtitle>
+      <Card.Text className='card-text'>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed commodo
+       est.Pellentesque eu placerat mauris. Etiam ut maximus ante. Suspendisse
+       varius dolor nec bibendum dignissim. Proin in diam a justo sollicitudin
+       mattis eu sagittisnunc. In hac habitasse.
       </Card.Text>
-      <Card.Link href="#">Card Link</Card.Link>
-      <Card.Link href="#">Another Link</Card.Link>
      </Card.Body>
     </Card>
-
-
     {/* card three */}
-    <Card className='sectionTow-cards'>
+    <Card className='sectionTow-card shadow rounded-3     border-0 text-center'>
      <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-      <Card.Text>
-       Some quick example text to build on the card title and make up the
-       bulk of the card's content.
+      <Card.Subtitle className='card-subtitle fw-bold py-3'>Lorem ipsum dolor sit .</Card.Subtitle>
+      <Card.Text className='card-text'>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed commodo
+       est.Pellentesque eu placerat mauris. Etiam ut maximus ante. Suspendisse
+       varius dolor nec bibendum dignissim. Proin in diam a justo sollicitudin
+       mattis eu sagittisnunc. In hac habitasse.
       </Card.Text>
-      <Card.Link href="#">Card Link</Card.Link>
-      <Card.Link href="#">Another Link</Card.Link>
      </Card.Body>
     </Card>
-    
+
    </div>
   </div>
  );
